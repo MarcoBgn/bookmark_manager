9d5be53dd6d1970244a59deace7b69d2eb338b7c
Marco Bagnasco <admin@hecticmojo.com>
2016-02-09 16:31:08 +0000

    Created Sinatra Modular App - Created first test

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
+require './models/link.rb'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Hello BookmarkManager!'
+    redirect '/links'
   end
+  
+  get '/links' do
+    @links = Link.all
+    erb :index
+  end
+  
 
-  # start the server if ruby file executed directly
+
   run! if app_file == $0
 end
